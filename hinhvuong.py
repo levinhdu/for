3b9de05eb80da4f25e32ce9fbe321f2eafb451df
levinhdu <duledule1914@gmail.com>
2021-08-24 19:18:51 +0700

    hinhvuong.1.0

---
+++ hinhvuong.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Turtle()
+wn = turtle.Screen()
+wn.bgcolor("black")
+wn.title("Square")
+
+t.color("#FF0000")
+for i in range(4):
+    t.fd(100)
+    t.lt(90)
+turtle.done()
